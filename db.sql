fe65777e4d6e42eeba305ecfc216a2a7d085548d
eavnavas <ernestonavas53@gmail.com>
2024-12-27 18:00:48 -0600

    se creo una db

---
+++ db.sql
@@ -0,0 +1,10 @@
+-- Se creó una base de datos mysql
+CREATE DATABASE prueba_db;
+
+-- Se creó una tabla empleados
+CREATE TABLE empleados(
+    user_id INTEGER NOT NULL AUTO INCREMENT PRIMARY KEY
+    name TEXT
+    surname TEXT
+    birthday DATETIME
+);
